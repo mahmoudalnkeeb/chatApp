faf35fcb39f489661f6379a11681b313400ad6a5
Mahmoud Alnkeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2021-11-22 18:55:19 +0200

    admin

--- server.js
+++ server.js
@@ -1,54 +1,67 @@
-const port = process.env.PORT || 3000
+const { instrument } = require("@socket.io/admin-ui");
+const port = process.env.PORT || 3000;
 
-var http = require('http');
+var http = require("http");
 
-var express = require('express');
-const { log } = require('console');
+var express = require("express");
+const { log } = require("console");
 
 var app = express();
 
 var server = http.createServer(app);
 // Pass a http.Server instance to the listen method
-var io = require('socket.io')(server);
+var io = require("socket.io")(server, {
+  cors: {
+    origin: [
+      "http://127.0.0.1:3000/",
+      "https://alnakeeb-chat.herokuapp.com/",
+      "https://admin.socket.io/",
+    ],
+    credentials: true,
+  },
+});
 // The server should start listening
 server.listen(port);
 // Register the index route of your app that returns the HTML file
-app.use(express.static('./'))
+app.use(express.static("./"));
 
 /*-------------------------------------------server side --------------------------------------------------*/
 
-const users = {}
+const users = {};
 
 // Expose the node_modules folder as static resources (to access socket.io.js in the browser)
-app.use('/static', express.static('node_modules'));
+app.use("/static", express.static("node_modules"));
 
+io.on("connection", (socket) => {
+  //new user joined the chat handling serverside
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+    console.log(users);
+  });
 
-io.on('connection', socket => {
+  //new user leaved the chat handling serverside
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
 
+  //handling chat messages
+  socket.on("send-chat-message", (message, room) => {
+    if (room == "") {
+      socket.broadcast.emit("chat-message", {
+        message: message,
+        name: users[socket.id],
+        room: room,
+      });
+    } else {
+      socket.broadcast.to(room).emit("chat-message", {
+        message: message,
+        name: users[socket.id],
+        room: room,
+      });
+    }
+  });
+});
 
-
-
-    //new user joined the chat handling serverside
-    socket.on("new-user", name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
-
-    })
-
-    //new user leaved the chat handling serverside
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
-    })
-
-    //handling chat messages
-    socket.on('send-chat-message', (message, room) => {
-
-        if (room == '') {
-            socket.broadcast.emit('chat-message', { message: message, name: users[socket.id], room: room })
-        } else {
-            socket.broadcast.to(room).emit('chat-message', { message: message, name: users[socket.id], room: room })
-        }
-    })
-
-})
+instrument(io, { auth: false });
